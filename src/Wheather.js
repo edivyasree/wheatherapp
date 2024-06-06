9ecb344496bf8613d518cbf18499c12d537f171a
edivyasree <edivyasree@gmail.com>
2024-06-06 20:36:41 +0530

    first commit

---
+++ src/Wheather.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+
+const api = {
+  key: "85118da733519b7360aa5cd131cc7a07",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+export default function Weather() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp < 20
+            ? `app warm`
+            : `app`
+          : `app`
+      }
+      style={{
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <main>
+        <div className="search-box">
+        <h1>Wheather App</h1>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+            style={{
+              padding: "10px",
+              fontSize: "16px",
+              width: "200px",
+              borderRadius: "5px",
+              border: "1px solid #ccc",
+            }}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)} &#8451;
+              </div>
+              <div className="weather">
+                {weather.weather[0].main}
+                <h4>Humidity: {weather.main.humidity}%</h4>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
+}
